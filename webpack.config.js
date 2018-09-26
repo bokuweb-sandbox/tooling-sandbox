71c42531eeb13d57db55fefd4663753fc9de3998
Satoshi Ueki <ueki@SatoshinoMacBook-puro.local>
2018-09-26 11:16:45 +0900

    feat: Add min example

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+const mode = (() => {
+  const nEnv = process.env.NODE_ENV;
+  switch (nEnv) {
+    case "production":
+    case "development":
+      return nEnv;
+    default:
+      return "development";
+  }
+})();
+
+const isDevMode = () => mode === DEV_MODE;
+
+module.exports = {
+  mode,
+  entry: ["./src/index.tsx"],
+  output: {
+    path: `${__dirname}/dist`,
+    historyApiFallback: true,
+    filename: "[name].js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(process.cwd(), "public", "index.html")
+    })
+  ],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  }
+};
